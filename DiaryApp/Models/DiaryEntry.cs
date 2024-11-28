1c0c469e46f5b87cd8ed5ff3ccc00beda7467f58
Umut Saw <umut1tr@hotmail.com>
2024-11-28 21:27:25 +0100

    added DiaryEntry Model + added dependencies for EntityFramework + added connection string to DB

---
+++ DiaryApp/Models/DiaryEntry.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DiaryApp.Models
+{
+    public class DiaryEntry
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public  string Title { get; set; } = string.Empty;
+        [Required]
+        public string Content { get; set; } = string.Empty;
+        [Required]
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+
+    }
+}
