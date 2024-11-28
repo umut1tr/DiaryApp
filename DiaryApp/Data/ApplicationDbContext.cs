ef3d8cae56652877e32c7b99eee6aa0f7ad7493b
Umut Saw <umut1tr@hotmail.com>
2024-11-28 22:15:58 +0100

    add ApplicationDbContext + register DbContext in DI Container.

---
+++ DiaryApp/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace DiaryApp.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+
+        // constructor to pass the ApplicationDbContext options to the base DbContext which we inherit
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base (options)
+        {
+            
+        }
+
+    }
+}
