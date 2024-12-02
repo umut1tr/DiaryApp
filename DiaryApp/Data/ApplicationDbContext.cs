41d268b2171a4a9574b03bc2cb0acd4d67c21ab3
Umut Saw <umut1tr@hotmail.com>
2024-12-02 18:36:54 +0100

    added migration + DbSet

--- DiaryApp/Data/ApplicationDbContext.cs
+++ DiaryApp/Data/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using DiaryApp.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace DiaryApp.Data
 {
@@ -10,6 +11,8 @@ namespace DiaryApp.Data
         {
             
         }
+
+        public DbSet<DiaryEntry> DiaryEntries { get; set; }
 
     }
 }
